bd01e397bcdbe2198320bf36054b93c9faecd856
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-01-19 11:41:19 +0100

    Refactor Code final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_1_SPRINT_7';
-}
+  
+  check_checkbox(id:string){
+    const option_selected = <HTMLInputElement> document.getElementById("col_"+id);
+    let price = <HTMLInputElement> document.getElementById("price");
+    if (check_clicked.checked ==  true) {
+      if (id == "0"){
+        const price_total = parseInt(price.textContent!) + parseInt();
+        price.textContent = String(price_total);
+      } 
+      if (id == "1"){
+        const price_total = parseInt(price.textContent!) + 300;
+        price.textContent = String(price_total);
+      } 
+      if (id == "2"){
+        const price_total = parseInt(price.textContent!) + 200;
+        price.textContent = String(price_total);
+      } 
+      
+    } else {
+      if (id == "0"){
+        const price_total = parseInt(price.textContent!) - 500;
+        price.textContent = String(price_total);
+      } 
+      if (id == "1"){
+        const price_total = parseInt(price.textContent!) - 300;
+        price.textContent = String(price_total);
+      } 
+      if (id == "2"){
+        const price_total = parseInt(price.textContent!) - 200;
+        price.textContent = String(price_total);
+      } 
+    } 
+
+    }
+  }
+
